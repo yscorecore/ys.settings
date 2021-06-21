736aaeb41847bd6195ada45f1f51129a6e36a77f
yangpengbo <obpt@sina.com>
2021-06-21 13:37:48 +0800

    add default setting service impl frame

--- src/YS.Settings.Core/Class1.cs
+++ src/YS.Settings.Core/Class1.cs
@@ -1,15 +1,38 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using YS.AppContext;
 using YS.Knife.Data;
 
 namespace YS.Settings
 {
-    public interface ISettingService
+    public interface ISettingService:IUserSettingService,IAppSettingService
     {
-        Task<SettingItem> GetSetting(string contextKey, string settingKey);
 
-        Task SaveSetting(string contextKey, string settingKey, string settingValue);
+        Task<UserOrAppSetting> GetUserOrAppSetting(string settingKey);
+
+    }
+
+    public interface IUserSettingService
+    {
+        Task<SettingItem> GetUserSetting(string settingKey);
+
+        Task SaveUserSetting(SettingItem settingItem);
+
+        Task<bool> DeleteUserSetting(string settingKey);
+
+        Task<LimitData<SettingItem>> ListUserSettings(LimitInfo limitInfo);
+    }
+
+    public interface IAppSettingService
+    {
+        Task<SettingItem> GetAppSetting(string settingKey);
+
+        Task SaveAppSetting(SettingItem settingItem);
+        
+        Task<bool> DeleteAppSetting(string settingKey);
+
+        Task<LimitData<SettingItem>> ListAppSettings(LimitInfo limitInfo);
     }
 
     public class SettingItem
@@ -19,11 +42,23 @@ namespace YS.Settings
         public string Context { get; set; }
     }
 
+    public class UserOrAppSetting
+    {
+        public SettingItem UserSetting { get; set; }
+        public SettingItem AppSetting { get; set; }
+    }
+
     public static class SettingServiceExtensions
     {
         private const string JSON_CONTENT_TYPE = "JSON";
         private const string TEXT_CONTENT_TYPE = "TEXT";
+
+        private static T DeSerialize<T>(SettingItem settingItem)
+        {
+            return default;
+        }
         
+
         private static T DeSerialize<T>(string contentType,string content)
         {
             switch (contentType.ToUpper())
@@ -37,25 +72,62 @@ namespace YS.Settings
             }
             
         }
-
-        public static async Task<T> GetSetting<T>(this ISettingService settingService, string contextKey, string settingKey)
+        private static string Serialize<T>(T content)
         {
-            var item = await settingService.GetSetting(contextKey, settingKey);
+            return null;
+
+        }
+
+        private static T Merge<T>(T source, T target)
+        {
+            return default;
+        }
+
+        public static async Task<T> GetUserSetting<T>(this IUserSettingService userSettingService, string settingKey)
+        {
+            var item = await userSettingService.GetUserSetting(settingKey);
             
             return item==null?default:DeSerialize<T>(item.ContentType,item.Context);
         }
-
-        public  static async Task SaveSetting<T>(this ISettingService settingService, string contextKey, string settingKey, T settingValue)
+        
+        public  static async Task SaveUserSetting<T>(this IUserSettingService userSettingService, string settingKey, T settingValue)
         {
-            string val = settingValue.ToJsonString();
-            await settingService.SaveSetting(contextKey, settingKey, val);
+            if (settingValue is string str)
+            {
+                await userSettingService.SaveUserSetting(new SettingItem{ ContentKey = settingKey,ContentType = TEXT_CONTENT_TYPE,  Context = str } );
+            }
+            else
+            {
+                await userSettingService.SaveUserSetting(new SettingItem{ ContentKey = settingKey,ContentType = JSON_CONTENT_TYPE,  Context = Serialize(settingValue) } );
+            }
+        }
+        public static async Task<T> GetAppSetting<T>(this IAppSettingService appSettingService, string settingKey)
+        {
+            var item = await appSettingService.GetAppSetting(settingKey);
+            
+            return item==null?default:DeSerialize<T>(item.ContentType,item.Context);
+        }
+        
+        public  static async Task SaveAppSetting<T>(this IAppSettingService appSettingService, string settingKey, T settingValue)
+        {
+            if (settingValue is string str)
+            {
+                await appSettingService.SaveAppSetting(new SettingItem{ ContentKey = settingKey,ContentType = TEXT_CONTENT_TYPE,  Context = str } );
+            }
+            else
+            {
+                await appSettingService.SaveAppSetting(new SettingItem{ ContentKey = settingKey,ContentType = JSON_CONTENT_TYPE,  Context = Serialize(settingValue) } );
+            }
+
         }
 
-        public static Task<T> GetUserSetting<T>(this ISettingService settingService, string settingKey) =>
-            settingService.GetSetting<T>(AppContextKeys.UserId, settingKey);
-
-        public static Task SaveUserSetting<T>(this ISettingService settingService, string settingKey, T settingValue) =>
-            settingService.SaveSetting(AppContextKeys.UserId, settingKey, settingValue);
+        public static async Task<T> GetUserOrAppSetting<T>(this ISettingService settingService, string settingKey)
+        {
+            var userOrAppSetting = await settingService.GetUserOrAppSetting(settingKey);
+            var appValue = DeSerialize<T>(userOrAppSetting.AppSetting);
+            var userValue = DeSerialize<T>(userOrAppSetting.UserSetting);
+            return Merge(appValue, userValue);
+        }
 
     }
 
---
+++ src/YS.Settings.Impl.Default/DefaultSettingService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using YS.AppContext;
+using YS.Knife;
+using YS.Knife.Data;
+
+namespace YS.Settings.Impl.Default
+{
+    [Service(typeof(IAppSettingService))]
+    [Service(typeof(IUserSettingService))]
+    [Service(typeof(ISettingService))]
+    public class DefaultSettingService: ISettingService
+    {
+        private readonly IAppContext _appContext;
+
+        public DefaultSettingService(IAppContext appContext)
+        {
+            this._appContext = appContext;
+        }
+
+        public Task<SettingItem> GetUserSetting(string settingKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveUserSetting(SettingItem settingItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteUserSetting(string settingKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LimitData<SettingItem>> ListUserSettings(LimitInfo limitInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SettingItem> GetAppSetting(string settingKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveAppSetting(SettingItem settingItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAppSetting(string settingKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LimitData<SettingItem>> ListAppSettings(LimitInfo limitInfo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
